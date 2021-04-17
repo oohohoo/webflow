183fad367662319bb703dc27cf4bf5ca28876f37
dejan-77 <dejankutic@me.com>
2021-04-17 17:02:25 +0200

    ht

--- fogmodule-scrolltrigger.js
+++ fogmodule-scrolltrigger.js
@@ -543,7 +543,7 @@ SHOW HIDE HEADER ON SCROLL + CUSTOM ANIMATION
 showAnim
 .to(".logo-frka", {autoAlpha:0 }, 0)
 .to(".navitem", {yPercent: 50, autoAlpha:0, stagger: 0.05},"<0.1")
-.to(".kontakt", {backgroundColor: "transparent", x:30},"<0.2").progress(1);
+.to(".kontakt", {x:30},"<0.2").progress(1);
 
 var elementFirst = document.querySelector('.kontakt');
 
