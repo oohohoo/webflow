c7c462ad85248780a715b01f3ce4390de1532c00
dejan-77 <dejankutic@me.com>
2021-04-17 17:28:39 +0200

    ht

--- fogmodule-scrolltrigger.js
+++ fogmodule-scrolltrigger.js
@@ -545,7 +545,7 @@ showAnim
 .set(".logo-frka", {autoAlpha:1 })
 .to(".logo-frka", {autoAlpha:0 }, 0)
 .fromTo(".navitem", {yPercent: 0, autoAlpha:1}, {yPercent: 50, autoAlpha:0, stagger: 0.05},"<0.1")
-.to(".kontakt", {x:30},"<0.2").progress(1);
+.to(".kontakt", {backgroundColor: "transparent", x:30},"<0.2").progress(1);
 
 var elementFirst = document.querySelector('.kontakt');
 
