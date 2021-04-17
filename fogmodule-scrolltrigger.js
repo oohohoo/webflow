239414b3264c3d7bc6c5868e00a6b6923cac3a11
dejan-77 <dejankutic@me.com>
2021-04-17 11:17:56 +0200

    ht

--- fogmodule-scrolltrigger.js
+++ fogmodule-scrolltrigger.js
@@ -491,11 +491,11 @@ TEXT REVEAL ANIMATION SPLIT BY LETTER - cameron knight
 
 
    tl.set(element, { autoAlpha: 1 });
-   tl.from(letters, 2, {
-     transformOrigin: "top",
-     rotationX: -90,
-     x: 30,
+   tl.from(letters, 1.6, {
+     transformOrigin: "center",
+     rotationY: 90,
+     x: 60,
     ease: CustomEase.create("custom", "M0,0 C0.425,0.005 0,1 1,1 "),
-     stagger: 0.01 });
+     stagger: 0.025 });
 
  });
