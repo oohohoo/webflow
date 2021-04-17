4506454114190892ccaaf47a5f4074fd0e9871a2
dejan-77 <dejankutic@me.com>
2021-04-17 09:51:49 +0200

    f

--- fogmodule-scrolltrigger.js
+++ fogmodule-scrolltrigger.js
@@ -93,7 +93,7 @@ revealContainers.forEach((container) => {
     scrollTrigger: {
       trigger: container,
        scroller: ".smooth-scroll",
-       invalidateOnRefresh: true,
+      
       toggleActions: "restart none none reset"
     }
   });
