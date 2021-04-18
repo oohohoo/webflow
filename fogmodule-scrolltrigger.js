b7ccb53fa90c85293d4d4725ba9d5346c7fffee7
dejan-77 <dejankutic@me.com>
2021-04-18 14:31:50 +0200

    f

--- fogmodule-scrolltrigger.js
+++ fogmodule-scrolltrigger.js
@@ -554,30 +554,33 @@ tl5.from(cover.querySelector('img'), {
 SVG CURVED TRANSITION
 ================================================================================ */
 
-let svganima = gsap.timeline( {
-  paused: true,
-     scrollTrigger: {
-    scroller: ".smooth-scroll",
-    trigger: ".svgcurve-wrap",   
-    start: 'top top',
-    //end: "bottom bottom", 
-     scrub: true,
-  },
 
 
-});
-
-let $path = document.querySelector(".path"),
-    $logo = document.querySelector(".logo"),
-    repeat = false,
+let path = document.querySelector(".path"),
+    /* logo = document.querySelector(".logo"), */
+    /* repeat = false, */
     animate = () => {
       const start = "M 0 100 V 50 Q 50 0 100 50 V 100 z";
       const end = "M 0 100 V 0 Q 50 0 100 0 V 100 z";
       
+      let svganima = gsap.timeline( {
+        paused: true,
+           scrollTrigger: {
+          scroller: ".smooth-scroll",
+          trigger: ".svgcurve-wrap",   
+          start: 'top top',
+          //end: "bottom bottom", 
+           scrub: true,
+           toggleActions: "restart none none reset"
+        },
+      
+      
+      });
+
       svganima
       /* new TimelineMax(repeat ? { paused: true } : {repeat: 1, repeatDelay: 1}) */
-        .to($path, 0.8, {attr: { d: start }, ease: Power2.easeIn})
-        .to($path, 0.4, {attr: { d: end }, ease: Power2.easeOut})
+        .to(path, 0.8, {attr: { d: start }, ease: Power2.easeIn})
+        .to(path, 0.4, {attr: { d: end }, ease: Power2.easeOut})
         /* .from($logo, .8, {y: 75}, '-=.8') */
         .play(0);
     };
