013c061402d3f6e6f6e5007e8b6926892db28e2b
dejan-77 <dejankutic@me.com>
2021-04-18 11:42:19 +0200

    gr

--- fogmodule-scrolltrigger.js
+++ fogmodule-scrolltrigger.js
@@ -554,17 +554,20 @@ tl5.from(cover.querySelector('img'), {
 SVG CURVED TRANSITION
 ================================================================================ */
 
-let path = document.querySelector(".path"),
-    logo = document.querySelector(".logo"),
+let svganima = gsap.timeline()
+
+let $path = document.querySelector(".path"),
+    $logo = document.querySelector(".logo"),
     repeat = false,
     animate = () => {
       const start = "M 0 100 V 50 Q 50 0 100 50 V 100 z";
       const end = "M 0 100 V 0 Q 50 0 100 0 V 100 z";
-
-      new TimelineMax(repeat ? { paused: true } : {repeat: -1, repeatDelay: 1})
-        .to(path, 0.8, {attr: { d: start }, ease: Power2.easeIn})
-        .to(path, 0.4, {attr: { d: end }, ease: Power2.easeOut})
-        .from(logo, .8, {y: 75}, '-=.8')
+      
+      svganima
+      /* new TimelineMax(repeat ? { paused: true } : {repeat: 1, repeatDelay: 1}) */
+        .to($path, 0.8, {attr: { d: start }, ease: Power2.easeIn})
+        .to($path, 0.4, {attr: { d: end }, ease: Power2.easeOut})
+        .from($logo, .8, {y: 75}, '-=.8')
         .play(0);
     };
 
