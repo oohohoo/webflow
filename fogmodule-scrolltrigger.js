f85762a16c9665340d1b257eff5b289929bf4420
dejan-77 <dejankutic@me.com>
2021-04-18 11:04:48 +0200

    fe

--- fogmodule-scrolltrigger.js
+++ fogmodule-scrolltrigger.js
@@ -550,3 +550,21 @@ tl5.from(cover.querySelector('img'), {
   clipPath: 'inset(10%)',
 })
 
+  /* ============================================================================
+SVG CURVED TRANSITION
+================================================================================ */
+
+let $path = document.querySelector(".path"),
+    $logo = document.querySelector(".logo"),
+    repeat = false,
+    animate = () => {
+      const start = "M 0 100 V 50 Q 50 0 100 50 V 100 z";
+      const end = "M 0 100 V 0 Q 50 0 100 0 V 100 z";
+      new TimelineMax(repeat ? { paused: true } : {repeat: -1, repeatDelay: 1})
+        .to($path, 0.8, {attr: { d: start }, ease: Power2.easeIn})
+        .to($path, 0.4, {attr: { d: end }, ease: Power2.easeOut})
+        .from($logo, .8, {y: 75}, '-=.8')
+        .play(0);
+    };
+
+animate();
